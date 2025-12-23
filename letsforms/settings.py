c3a03e7ec97d13112f4b48f0ad71b5ecc30a5264
60ilya <pybuuuk@gmail.com>
2025-12-23 23:00:53 +1000

    Add secure proxy to settings

--- letsforms/settings.py
+++ letsforms/settings.py
@@ -199,6 +199,9 @@ CORS_ALLOW_METHODS = [
     'PUT',
 ]
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
